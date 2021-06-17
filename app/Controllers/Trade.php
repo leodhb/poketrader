a041da294ac787e90c286a48a96276ddb9772172
leodhb <henrikleod@gmail.com>
2021-06-17 18:10:11 -0300

    Remove TradeController constructor

--- app/Controllers/Trade.php
+++ app/Controllers/Trade.php
@@ -5,10 +5,6 @@ namespace Controllers;
 use Libraries\Controller;
 
 class Trade extends Controller{
-    public function __construct()
-    {
-        
-    }
 
     public function index() {
         $this->view('pages/home');
