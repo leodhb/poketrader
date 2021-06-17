ce0383f3143879c2cb4075d86d71d4b029fac444
leodhb <henrikleod@gmail.com>
2021-06-17 19:31:03 -0300

    Some database management functions

--- app/Libraries/Database.php
+++ app/Libraries/Database.php
@@ -10,7 +10,7 @@ class Database {
     private $password = 'cG9rZXRyYWRlcmJlbW1hc3Nh';
     private $db_name = 'poketrader_leod';
     private $connection;
-    
+    private $stmt;
 
     public function __construct() {
         $options = [
@@ -22,6 +22,45 @@ class Database {
         } catch (PDOException $e) {
             die('Erro: ' . $e->getMessage());
         }
-        
     }
+
+    public function query($sql) {
+        $this->stmt = $this->connection->prepare($sql);
+    }
+
+    public function bind($parameter, $value, $type = null) {
+        if(is_null($type)) {
+            switch(true) {
+                case is_int($value):
+                    $type = PDO::PARAM_INT;
+                    break;
+                case is_bool($value):
+                    $type = PDO::PARAM_BOOL;
+                    break;
+                case is_null($value): 
+                    $type = PDO::PARAM_NULL;
+                    break;
+                default:
+                    $type = PDO::PARAM_STR;
+            }
+        }
+
+        $this->stmt->bindValue($parameter, $value, $type);
+    }
+
+    public function execute() {
+        return $this->stmt->execute();
+    }
+
+    public function fetch() {
+        $this->execute();
+        return $this->stmt->fetch(PDO::FETCH_OBJ);
+    }
+
+    public function fetchAll() {
+        $this->execute();
+        return $this->stmt->fetchAll(PDO::FETCH_OBJ);
+    }
+
+    //that's all. We don't need to use some functions like rowCount or lastIndex
 }
