1c1e7e7b98b2ad7454356ddede3f300d2b48cfa7
leodhb <henrikleod@gmail.com>
2021-06-18 04:26:18 -0300

    Creating trade model

---
+++ app/Models/Trade.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Models;
+
+use Libraries\Database;
+
+class Trade {
+    private $db;
+
+    public function __construct()
+    {
+        $this->db = new Database();
+    }
+
+    public function trade($data) {
+        $this->db->query('INSERT INTO trades (player_1, player_2, trade_data) VALUES (:player1, :player2, :trade_data)');
+        $this->db->bind('player1', $data['player1']);
+        $this->db->bind('player2', $data['player2']);
+        $this->db->bind('trade_data', $data['trade_data']);
+        $result = $this->db->execute() ? true : false;
+        return $result;
+    }
+
+    public function getHistory() {
+        $this->db->query('SELECT * FROM trades');
+        return $this->db->fetchAll();
+    }
+}
