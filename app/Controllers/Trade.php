17797bc135663b3e7c3b7a25028d6c2234df79ac
leodhb <henrikleod@gmail.com>
2021-06-18 04:26:51 -0300

    Add INSERT trade functionality

--- app/Controllers/Trade.php
+++ app/Controllers/Trade.php
@@ -1,12 +1,31 @@
 <?php
 
 namespace Controllers;
-
 use Libraries\Controller;
 
 class Trade extends Controller{
+    
+    private $tradeModel;
+    
+    public function __construct()
+    {
+       $this->tradeModel = $this->model('Trade'); 
+    }
 
     public function index() {
+        $trade = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+        if(isset($trade)) {
+
+            $data = [
+                'player1' => trim($trade['player1']),
+                'player2' => trim($trade['player2']),
+                'trade_data' => trim($trade['trade_data'])
+            ];
+            var_dump($trade);
+            $this->tradeModel->trade($data);
+        }
+
         $this->view('pages/home');
     }
 
---
+++ app/Views/pages/home.php
@@ -0,0 +1,18 @@
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-lg-6 col-12">
+            <form action="" method="post">
+                <div class="form-group">
+                    <input type="text" name="player1" id="player1" class="form-control" placeholder="player 1">
+                </div>
+                <div class="form-group">
+                    <input type="text" name="player2" id="player2" class="form-control" placeholder="player 2">
+                </div>
+                <div class="form-group">
+                    <input type="text" name="trade_data" id="trade_data" class="form-control" placeholder="trade data">
+                </div>
+                <button type="submit">lansar</button>
+            </form>
+        </div>
+    </div>
+</div>
