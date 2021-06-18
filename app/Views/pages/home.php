03ee086b4456917bea06c393aae7a23cfef47d0c
leodhb <henrikleod@gmail.com>
2021-06-18 20:09:53 -0300

    Main page structure

--- app/Views/pages/home.php
+++ app/Views/pages/home.php
@@ -1,49 +1,51 @@
 <div class="container">
     <div class="row justify-content-center">
-            <div class="col-md-6 text-center p-3">
-                <div class="pokedex-outer">
-                    <div class="pokedex pokedex-red">
-                        <h1>PLAYER 1</h1>
-                        <div class="row p-1 search-wrapper">
-                            <div class="col-9 p-0">
-                                <input type="text" id="search_input_p1" class="form-control search-input" placeholder="Pesquisar">
-                            </div>
-                            <div class="col-3 p-0">
-                                <button class="btn btn-primary w-100 search-btn" id="search_btn_p1">Inserir</button>
-                            </div>
+        <div class="col-md-6 text-center p-3">
+            <div class="pokedex-outer">
+                <div class="pokedex pokedex-red">
+                    <h1>PLAYER 1</h1>
+                    <div class="row p-1 search-wrapper">
+                        <div class="col-9 p-0">
+                            <input type="text" id="search_input_p1" class="form-control search-input" placeholder="Pesquisar">
+                        </div>
+                        <div class="col-3 p-0">
+                            <button class="btn btn-primary w-100 search-btn" id="search_btn_p1">Inserir</button>
                         </div>
                     </div>
-                    <div class="row pokedex-list p-4" id="pokedex_p1">
-                        
-                    </div>
-
                 </div>
+                <div class="row pokedex-list p-4" id="pokedex_p1">
+                </div>
+                <hr>
+                <div class="row pb-3 justify-content-center" id="display_xp_sum_p1"></div>
             </div>
+        </div>
 
-            <div class="col-md-6 text-center p-3">
-                <div class="pokedex-outer">
-                    <div class="pokedex pokedex-blue">
-                        <h1>PLAYER 2</h1>
-                        <div class="row p-1 search-wrapper">
-                            <div class="col-9 p-0">
-                                <input type="text" id="search_input_p2" class="form-control search-input" placeholder="Pesquisar">
-                            </div>
-                            <div class="col-3 p-0">
-                                <button class="btn btn-primary w-100 search-btn" id="search_btn_p2">Inserir</button>
-                            </div>
+        <div class="col-md-6 text-center p-3">
+            <div class="pokedex-outer">
+                <div class="pokedex pokedex-blue">
+                    <h1>PLAYER 2</h1>
+                    <div class="row p-1 search-wrapper">
+                        <div class="col-9 p-0">
+                            <input type="text" id="search_input_p2" class="form-control search-input" placeholder="Pesquisar">
                         </div>
-                    </div> 
-
-                    <div class="row pokedex-list p-4" id="pokedex_p2">
-                        
+                        <div class="col-3 p-0">
+                            <button class="btn btn-primary w-100 search-btn" id="search_btn_p2">Inserir</button>
+                        </div>
                     </div>
                 </div>
+
+                <div class="row pokedex-list p-4" id="pokedex_p2">
+
+                </div>
+                <hr>
+                <div class="row pb-3 justify-content-center" id="display_xp_sum_p2"></div>
             </div>
-            <div class="col-12">
-                    <div class="form-group">
-                        <input type="text" name="trade_data" id="trade_data" class="form-control" placeholder="trade data">
-                    </div>
-                    <button type="submit">lansar</button>
-            </div>        
+        </div>
+        <div class="col-12 text-center">
+            <p id="display_xp_conclusion"></p>
+            <div class="form-group">
+                <input type="text" name="trade_data" id="trade_data" class="form-control" placeholder="trade data">
+            </div>
+        </div>
     </div>
 </div>
