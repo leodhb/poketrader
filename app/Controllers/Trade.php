ee75bd133c54f9e17b78911a5c7f59d4d2f7811a
leodhb <henrikleod@gmail.com>
2021-06-19 17:50:41 -0300

    Clening out debug calls

--- app/Controllers/Trade.php
+++ app/Controllers/Trade.php
@@ -16,18 +16,19 @@ class Trade extends Controller{
         $trade = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
 
         if(isset($trade)) {
+
             $data = [
                 'player1' => trim($trade['player1']),
                 'player2' => trim($trade['player2']),
                 'trade_data' => trim($trade['trade_data'])
             ];
-            var_dump($trade);
             $this->tradeModel->trade($data);
         }
+
         $this->view('pages/home');
     }
-    
-    public function history()
+
+    public function history() 
     {
         $data = $this->tradeModel->getHistory();
         $this->view('pages/history', $data);
