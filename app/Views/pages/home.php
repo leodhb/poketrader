4b08de2ec344423c20a894e86210e2d128f6acb7
leodhb <henrikleod@gmail.com>
2021-06-19 17:51:00 -0300

    Improving homepage layout

--- app/Views/pages/home.php
+++ app/Views/pages/home.php
@@ -1,4 +1,11 @@
 <div class="container">
+    <header class="row poketrader-header">
+        <div class="col-12 text-center mt-4 mb-2">
+            <img src="img/poketrader-logo.png" alt="Logomarca do Pokemon Trade" class="branding img-fluid">
+            <h1>Bem-vindo ao PokeTrader!</h1>
+            <p class="header-subtitle">Aqui você e seu amigo podem trocar 1 a 6 pokémons cada</p>
+        </div>
+    </header>
     <div class="row justify-content-center">
         <div class="col-md-6 text-center p-3">
             <div class="pokedex-outer">
@@ -6,10 +13,10 @@
                     <h1>PLAYER 1</h1>
                     <div class="row p-1 search-wrapper">
                         <div class="col-9 p-0">
-                            <input type="text" id="search_input_p1" class="form-control search-input" placeholder="Pesquisar">
+                            <input type="text" id="search_input_p1" class="form-control search-input" placeholder="Procure por um Pokémon">
                         </div>
                         <div class="col-3 p-0">
-                            <button class="btn btn-primary w-100 search-btn" id="search_btn_p1">Inserir</button>
+                            <button class="btn btn-primary w-100 search-btn" id="search_btn_p1"><i class="fas fa-plus"></i> Adicionar</button>
                         </div>
                     </div>
                 </div>
@@ -26,16 +33,15 @@
                     <h1>PLAYER 2</h1>
                     <div class="row p-1 search-wrapper">
                         <div class="col-9 p-0">
-                            <input type="text" id="search_input_p2" class="form-control search-input" placeholder="Pesquisar">
+                            <input type="text" id="search_input_p2" class="form-control search-input" placeholder="Procure por um Pokémon">
                         </div>
                         <div class="col-3 p-0">
-                            <button class="btn btn-primary w-100 search-btn" id="search_btn_p2">Inserir</button>
+                            <button class="btn btn-primary w-100 search-btn" id="search_btn_p2"><i class="fas fa-plus"></i> Adicionar</button>
                         </div>
                     </div>
                 </div>
 
                 <div class="row pokedex-list p-4" id="pokedex_p2">
-
                 </div>
                 <hr>
                 <div class="row pb-3 justify-content-center" id="display_xp_sum_p2"></div>
@@ -43,9 +49,7 @@
         </div>
         <div class="col-12 text-center">
             <p id="display_xp_conclusion"></p>
-            <div class="form-group">
-                <input type="text" name="trade_data" id="trade_data" class="form-control" placeholder="trade data">
-            </div>
+            <button class="btn btn-primary" id="trade_btn">TROCAR</button>
         </div>
     </div>
 </div>
