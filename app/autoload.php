be78b3e1718eefcf820ff594553d5b6c7f135ba1
leodhb <henrikleod@gmail.com>
2021-06-19 21:42:42 -0300

    Improving autoloader

--- app/autoload.php
+++ app/autoload.php
@@ -1,4 +1,18 @@
 <?php
-    spl_autoload_register(function($class) {
-        require "$class.php";
-    });
+spl_autoload_register( function( $class ) {
+
+	$prefix = '';
+	$base_dir = __DIR__ . '/';
+	$len = strlen( $prefix );
+	if ( strncmp( $prefix, $class, $len ) !== 0 ) {
+		return;
+	}
+	$class_name = str_replace( $prefix, '', $class );
+	$file = $base_dir . str_replace('\\', '/', $class_name ) . '.php';
+
+	if( file_exists( $file ) ) {
+
+		require $file;
+	}
+
+});
